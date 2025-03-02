8a8da8af7831633b03c2c373cfd2122aef027102
earthcomfy <hanagetbel93@gmail.com>
2025-03-02 18:12:53 +0300

    fix: remove unnecessary files

--- tests/test_app/cms_plugins.py
+++ tests/test_app/cms_plugins.py
@@ -2,12 +2,9 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from django.utils.translation import gettext_lazy as _
 
-from .models import Container
-
 
 @plugin_pool.register_plugin
 class ContainerPlugin(CMSPluginBase):
     name = _("Container")
-    model = Container
     render_template = "test_app/container.html"
     allow_children = True
