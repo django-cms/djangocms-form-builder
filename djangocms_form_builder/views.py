0b037a0745d92e27085dfe0a0c8d179342fba73c
Fabian Braun <fsbraun@gmx.de>
2025-03-14 09:00:12 +0100

    Add some docstrings

--- djangocms_form_builder/views.py
+++ djangocms_form_builder/views.py
@@ -29,9 +29,30 @@ def register_form_view(cls, slug=None):
 
 
 class AjaxView(View):
-    # @method_decorator(csrf_exempt)
+    """
+    A Django view to handle AJAX requests for GET and POST methods for django CMS Form Builder forms.
+    this view allows django CMS plugins to receive ajax requests if they implement the `ajax_get` and
+    `ajax_post` methods. The form plugin implements the `ajax_post` method to handle form submissions.
+
+    Methods
+    -------
+
+    dispatch(request, \*args, \*\*kwargs)
+        Overrides the default dispatch method to handle AJAX requests.
+
+    decode_path(path)
+        Decodes a URL path into a dictionary of parameters.
+
+    plugin_instance(pk)
+        Retrieves the plugin instance and its associated model instance by primary key.
+
+    ajax_post(request, \*args, \*\*kwargs)
+        Handles AJAX POST requests. Calls the `ajax_post` method of the plugin or form instance if available.
+
+    ajax_get(request, \*args, \*\*kwargs)
+        Handles AJAX GET requests. Calls the `ajax_get` method of the plugin or form instance if available.
+    """
     def dispatch(self, request, *args, **kwargs):
-        # is_ajax = request.headers.get('X-Requested-With') == "XMLHttpRequest"
         if request.accepts("application/json"):
             if request.method == "GET" and "get" in self.http_method_names:
                 return self.ajax_get(request, *args, **kwargs)
@@ -63,6 +84,32 @@ class AjaxView(View):
         return plugin, instance
 
     def ajax_post(self, request, *args, **kwargs):
+        """
+        Handles AJAX POST requests for the form builder.
+
+        This method processes AJAX POST requests by determining the appropriate
+        plugin or form instance to handle the request based on the provided
+        keyword arguments.
+
+        Args:
+            request (HttpRequest): The HTTP request object.
+            \*args: Additional positional arguments.
+            \*\*kwargs: Additional keyword arguments, which may include:
+                - instance_id (int): The ID of the plugin instance.
+                - parameter (str): Optional parameter for decoding.
+                - form_id (str): The ID of the form instance.
+
+        Returns:
+            JsonResponse: A JSON response with the result of the AJAX POST request.
+            Http404: If the plugin or form instance cannot be found or does not
+                     support AJAX POST requests.
+
+        Raises:
+            Http404: If the plugin or form instance cannot be found or does not
+                     support AJAX POST requests.
+            ValidationError: If there is a validation error during the request
+                             processing.
+        """
         if "instance_id" in kwargs:
             plugin, instance = self.plugin_instance(kwargs["instance_id"])
             if hasattr(plugin, "ajax_post"):
@@ -90,6 +137,30 @@ class AjaxView(View):
         raise Http404()
 
     def ajax_get(self, request, *args, **kwargs):
+        """
+        Handles AJAX GET requests.
+
+        This method processes AJAX GET requests by checking for specific keys in the
+        `kwargs` and delegating the request to the appropriate handler.
+
+        Args:
+            request (HttpRequest): The HTTP request object.
+            \*args: Additional positional arguments.
+            \*\*kwargs: Additional keyword arguments.
+
+        Returns:
+            JsonResponse: A JSON response with the result of the AJAX request.
+            Http404: If the required keys are not found in `kwargs` or the handler is not available.
+
+        Raises:
+            ValidationError: If there is an error during the processing of the AJAX request.
+
+        Notes:
+            - If "instance_id" is present in `kwargs`, it attempts to retrieve the plugin instance
+              and calls its `ajax_get` method if available.
+            - If "form_id" is present in `kwargs`, it attempts to retrieve the form instance from
+              the `_formview_pool` and calls its `ajax_get` or `get` method if available.
+        """
         if "instance_id" in kwargs:
             plugin, instance = self.plugin_instance(kwargs["instance_id"])
             if hasattr(plugin, "ajax_get"):
