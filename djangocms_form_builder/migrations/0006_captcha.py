f158ace8ee564ccffe970692f9b5bb65a4a1ac1f
Fabian Braun <fsbraun@gmx.de>
2026-07-12 19:35:05 +0200

    fix: Migration order

--- djangocms_form_builder/migrations/0006_captcha.py
+++ djangocms_form_builder/migrations/0006_captcha.py
@@ -3,7 +3,7 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("djangocms_form_builder", "0004_alter_form_captcha_requirement_and_more"),
+        ("djangocms_form_builder", "0005_static_captcha_schema"),
     ]
 
     operations = [
